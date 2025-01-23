45ce9324920e05415bfdf8a153559b1cbd5a931a
Tim Flucker <timflucker@gmail.com>
2025-01-23 14:45:45 -0500

    updating console print statments

--- src/main/java/com/tim/blackjack/BlackjackApplication.java
+++ src/main/java/com/tim/blackjack/BlackjackApplication.java
@@ -1,13 +1,29 @@
 package com.tim.blackjack;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
-@SpringBootApplication
 public class BlackjackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlackjackApplication.class, args);
+
+		System.out.println("--------------------------------------\nWelcome to Tim's Blackjack game!\n--------------------------------------\n");
+		Scanner scanner = new Scanner(System.in);
+		boolean playing = true;
+		while(playing) {
+			new Game(scanner);
+			
+			System.out.println("\nContinue Playing?(Yes/No)");
+			String userInput = scanner.nextLine();
+			
+			if(userInput.equalsIgnoreCase("No")) {
+				playing = false;
+			} else if(!userInput.equalsIgnoreCase("Yes") || !userInput.equalsIgnoreCase("No")) {
+				System.out.println("Unrecognized input, closing game.");
+				playing = false;
+			}
+		}		
+		scanner.close();
+		System.out.println("\n--------------------------------------Thank you for playing!--------------------------------------");
 	}
 
 }
